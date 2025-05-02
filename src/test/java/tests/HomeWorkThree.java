87c7bd5263faf1f871d350a3c5ea57caf0565cef
Vasilenko  Natalia <natalya.vasilenko@sbermarket.ru>
2025-05-03 01:49:50 +0300

    commit for master

--- src/test/java/tests/HomeWorkThree.java
+++ src/test/java/tests/HomeWorkThree.java
@@ -2,15 +2,20 @@ package tests;
 
 import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byTagAndText;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$$;
+import static io.qameta.allure.Allure.step;
 
 @Tag("demoqa_n")
 
@@ -24,16 +29,33 @@ public class HomeWorkThree {
         Configuration.holdBrowserOpen = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
     }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+
+    }
+
 
     @Test
     void openSoftAssertions() {
 
+        step("Открываем главную страницу", () -> {
         open("");
+        });
+
+        step("Проверка", () -> {
+
         $(".HeaderMenu-nav").$(byText("Solutions")).hover();
         $(byText("Enterprises")).click();
         $$("#hero-section-brand-heading").shouldHave(CollectionCondition.texts("The AI-powered developer platform"));
-
+        });
 
 
     }
